785bf253a28305bc99aa7f785a59117640b0674d
Marcell Toth <tothmarcell97@gmail.com>
2017-10-21 16:30:54 +0200

    RefreshService file

---
+++ NeptunLight/NeptunLight.Android/RefreshService.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using JetBrains.Annotations;
+
+namespace NeptunLight.Droid
+{
+    public class RefreshService : Service
+    {
+        [CanBeNull]
+        public override IBinder OnBind([NotNull] Intent intent)
+        {
+            return null;
+        }
+    }
+}
