92e3272cc06a23b9ff2e737013aa61cfc9da52d5
Marcell Toth <tothmarcell97@gmail.com>
2017-11-03 12:42:07 +0100

    Target framework to 8.0, added BME

--- NeptunLight/NeptunLight/DataAccess/StaticData.cs
+++ NeptunLight/NeptunLight/DataAccess/StaticData.cs
@@ -9,6 +9,7 @@ namespace NeptunLight.DataAccess
         public IEnumerable<Institute> GetAvaialbleInstitutes()
         {
             yield return new Institute("BCE - Budapesti Corvinus Egyetem", new Uri("https://neptun3r.web.uni-corvinus.hu/hallgatoi_2/"));
+            yield return new Institute("BME - Budapesti Műszaki és Gazdaságtudományi Egyetem", new Uri("https://frame.neptun.bme.hu/hallgatoi/"));
         }
     }
 }
