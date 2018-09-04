6c8305af4868e8c4562436e1a0eefcd9a17f29da
Marcell Toth <marcell@marcelltoth.net>
2018-09-04 07:21:34 +0200

    Make the button on the settings page actually trigger a refresh

--- NeptunLight/NeptunLight.Android/Views/SettingsPage.cs
+++ NeptunLight/NeptunLight.Android/Views/SettingsPage.cs
@@ -1,9 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Android.App;
 using Android.OS;
 using Android.Preferences;
+using Android.Widget;
 using Autofac;
 using Microsoft.AppCenter.Analytics;
+using NeptunLight.Droid.Services;
 using NeptunLight.Services;
 
 namespace NeptunLight.Droid.Views
@@ -18,7 +22,29 @@ namespace NeptunLight.Droid.Views
             Preference logout = FindPreference("logout_button");
             logout.PreferenceClick += Logout_PreferenceClick;
 
+            Preference refresh = FindPreference("refresh_button");
+            refresh.PreferenceClick += Refresh_PreferenceClick;
+
             Analytics.TrackEvent("Settings page shown");
+        }
+
+        private async void Refresh_PreferenceClick(object sender, Preference.PreferenceClickEventArgs e)
+        {
+            Analytics.TrackEvent("Forced sync started");
+            Toast.MakeText(Activity, "Frissítés folyamatban...", ToastLength.Long).Show();
+            try
+            {
+                await App.Container.Resolve<RefreshManager>().RefreshAsync();
+                Toast.MakeText(Activity, "Frissítés sikeres!", ToastLength.Short).Show();
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(Activity, "Hiba a frissítés során", ToastLength.Short).Show();
+                Analytics.TrackEvent("Forced sync error", new Dictionary<string, string>{
+                    {"Message", ex.Message},
+                    { "Trace", ex.StackTrace.Substring(0,64)}
+                });
+            }
         }
 
         private async void Logout_PreferenceClick(object sender, Preference.PreferenceClickEventArgs e)
