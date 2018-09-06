1a458c54779d3964ba0f040ac3fcadd7b38ba298
Marcell Toth <marcell@marcelltoth.net>
2018-09-06 07:56:01 +0200

    Add SE

--- NeptunLight/NeptunLight/DataAccess/StaticData.cs
+++ NeptunLight/NeptunLight/DataAccess/StaticData.cs
@@ -10,6 +10,7 @@ namespace NeptunLight.DataAccess
         {
             yield return new Institute("BCE - Budapesti Corvinus Egyetem", new Uri("https://neptun3r.web.uni-corvinus.hu/hallgatoi_2/"));
             yield return new Institute("BME - Budapesti Műszaki és Gazdaságtudományi Egyetem", new Uri("https://frame.neptun.bme.hu/hallgatoi/"));
+            yield return new Institute("SE - Semmelweis Egyetem", new Uri("https://neptunweb.semmelweis.hu/hallgato/"));
         }
     }
 }
