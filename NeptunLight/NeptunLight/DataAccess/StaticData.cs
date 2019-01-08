7a6d814f6cc37134a9b99bee4c287b2d2a7bb07e
Marcell Toth <marcell@marcelltoth.net>
2019-01-08 09:48:36 +0100

    Add OE

--- NeptunLight/NeptunLight/DataAccess/StaticData.cs
+++ NeptunLight/NeptunLight/DataAccess/StaticData.cs
@@ -10,6 +10,7 @@ namespace NeptunLight.DataAccess
         {
             yield return new Institute("BCE - Budapesti Corvinus Egyetem", new Uri("https://neptun3r.web.uni-corvinus.hu/hallgatoi_2/"));
             yield return new Institute("BME - Budapesti Műszaki és Gazdaságtudományi Egyetem", new Uri("https://frame.neptun.bme.hu/hallgatoi/"));
+            yield return new Institute("OE - Óbudai Egyetem", new Uri("https://neptun.uni-obuda.hu/hallgato/"));
             yield return new Institute("PE - Pannon Egyetem", new Uri("https://neptun11.uni-pannon.hu/hallgato/"));
             yield return new Institute("SE - Semmelweis Egyetem", new Uri("https://neptunweb.semmelweis.hu/hallgato/"));
             yield return new Institute("SZE - Széchenyi István Egyetem", new Uri("https://netw8.nnet.sze.hu/hallgato/"));
