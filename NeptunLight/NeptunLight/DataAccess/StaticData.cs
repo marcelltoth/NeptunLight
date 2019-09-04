2fdf978341d2feccef07a2641af7ef9fb4be2a34
Marcell Toth <marcell@marcelltoth.net>
2019-09-04 09:18:20 +0200

    Add DUE

--- NeptunLight/NeptunLight/DataAccess/StaticData.cs
+++ NeptunLight/NeptunLight/DataAccess/StaticData.cs
@@ -12,6 +12,7 @@ namespace NeptunLight.DataAccess
             yield return new Institute("BGE - Budapesti Gazdasági Egyetem", new Uri("https://neptun6.uni-bge.hu/hallgato/"));
             yield return new Institute("BME - Budapesti Műszaki és Gazdaságtudományi Egyetem", new Uri("https://frame.neptun.bme.hu/hallgatoi/"));
             yield return new Institute("DE - Debreceni Egyetem", new Uri("https://www-3.neptun.unideb.hu/hallgato/"));
+            yield return new Institute("DUE - Dunaújvárosi Egyetem", new Uri("https://nappw.dfad.duf.hu/hallgato/"));
             yield return new Institute("ELTE - Eötvös Loránd Tudományegyetem", new Uri("https://hallgato.neptun.elte.hu/"));
             yield return new Institute("KRE - Károli Gáspár Református Egyetem", new Uri("https://neptun.kre.hu/hallgato/"));
             yield return new Institute("NKE -  Nemzeti Közszolgálati Egyetem", new Uri("https://neptunweb.uni-nke.hu/hallgato/"));
